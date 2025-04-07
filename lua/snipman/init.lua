1aa1d7b4620c4722d8151c41c57087dec1c168b4
Stefan Schwaighofer <ste.schwaighofer@gmail.com>
2025-04-07 21:40:30 +0200

    refactor: snippets by filetype builder

--- lua/snipman/init.lua
+++ lua/snipman/init.lua
@@ -2,6 +2,30 @@ local Snippet = require("snipman.snippet")
 
 ---@alias snipman.SnippetByFileType table<string, table<[string, snipman.SnippetBody]>>
 
+---@class snipman.SnippetByFileTypeBuilder
+---@field snippets_by_ft snipman.SnippetByFileType
+local SnippetByFileTypeBuilder = {}
+
+function SnippetByFileTypeBuilder.new()
+	return setmetatable({
+		snippets_by_ft = {},
+	}, { __index = SnippetByFileTypeBuilder })
+end
+
+---@param filetype string
+---@param snippet snipman.Snippet
+function SnippetByFileTypeBuilder:add(filetype, snippet)
+	if not self.snippets_by_ft[filetype] then
+		self.snippets_by_ft[filetype] = {}
+	end
+
+	table.insert(self.snippets_by_ft[filetype], snippet)
+end
+
+function SnippetByFileTypeBuilder:build()
+	return self.snippets_by_ft
+end
+
 ---@class snipman.Instance
 ---@field snippets_by_ft table<string, snipman.Snippet[]>
 local M = {}
@@ -11,20 +35,16 @@ local M = {}
 
 ---@param opts snipman.SetupOpts
 function M.setup(opts)
-	---@type table<string, snipman.Snippet[]>
-	local snippets_by_ft = {}
-	for filetype, snippets in pairs(opts.snippets_by_ft or {}) do
-		if not snippets_by_ft[filetype] then
-			snippets_by_ft[filetype] = {}
-		end
+	local builder = SnippetByFileTypeBuilder.new()
 
+	for filetype, snippets in pairs(opts.snippets_by_ft or {}) do
 		for _, snippet_config in ipairs(snippets) do
 			local snippet = Snippet.new(snippet_config[1], snippet_config[2])
-			table.insert(snippets_by_ft[filetype], snippet)
+			builder:add(filetype, snippet)
 		end
 	end
 
-	M.snippets_by_ft = snippets_by_ft
+	M.snippets_by_ft = builder:build()
 end
 
 ---@return snipman.Snippet[]
