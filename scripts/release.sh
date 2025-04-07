79b0545e689da49bd40b87b3f184f572e823b1ec
Stefan Schwaighofer <ste.schwaighofer@gmail.com>
2025-04-07 21:52:24 +0200

    chore: prefix tags

--- scripts/release.sh
+++ scripts/release.sh
@@ -13,17 +13,18 @@ fi
 echo -e "${GREEN}updated changelog ✅${RESET}"
 
 VERSION=$(git cliff --bumped-version)
+TAG="v${VERSION}"
 
 git add CHANGELOG.md
 git commit -m "chore(release): prepare for $VERSION"
-git tag $VERSION
+git tag $TAG
 
 echo -e "${GREEN}tagged version ✅${RESET}"
 
 REMOTE=$(git remote)
 
 git push $REMOTE main
-git push $REMOTE $VERSION
+git push $REMOTE $TAG
 
 echo -e "${GREEN}pushed main + tag ✅${RESET}"
 
