3d3f3181c935f2cd5ae33a0657b9483397d5f7ff
Stefan Schwaighofer <ste.schwaighofer@gmail.com>
2025-04-07 21:46:28 +0200

    chore: setup build

---
+++ justfile
@@ -0,0 +1,5 @@
+@_default:
+    just --list
+
+release:
+    ./scripts/release.sh
---
+++ scripts/release.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+GREEN="\x1b[32m"
+RESET="\x1b[0m"
+
+BUMP=$(git cliff --bump -o CHANGELOG.md 2>&1)
+
+if echo "$BUMP" | grep -q "There is nothing to bump."; then
+    echo "nothing to release"
+    exit 1
+fi
+
+echo -e "${GREEN}updated changelog ✅${RESET}"
+
+VERSION=$(git cliff --bumped-version)
+
+git add CHANGELOG.md
+git commit -m "chore(release): prepare for $VERSION"
+git tag $VERSION
+
+echo -e "${GREEN}tagged version ✅${RESET}"
+
+REMOTE=$(git remote)
+
+git push $REMOTE main
+git push $REMOTE $VERSION
+
+echo -e "${GREEN}pushed main + tag ✅${RESET}"
+
