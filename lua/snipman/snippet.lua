653da575f1ed5d4ac00671c90ef34ef671f885a7
Stefan Schwaighofer <ste.schwaighofer@gmail.com>
2025-08-03 16:59:45 +0200

    chore: inline evaluate

--- lua/snipman/snippet.lua
+++ lua/snipman/snippet.lua
@@ -16,24 +16,18 @@ end
 
 ---@return string
 function Snippet:evaluate()
-	local snippet = self:to_string()
-	return vim.trim(snippet)
-end
-
----@return string
-function Snippet:to_string()
 	if type(self.body) == "string" then
 		local body = self.body --[[@as string]]
-		return body
+		return vim.trim(body)
 	end
 
 	if type(self.body) == "table" then
 		local body = self.body --[[@as table<string>]]
-		return table.concat(body, "\n")
+		return vim.trim(table.concat(body, "\n"))
 	end
 
 	if type(self.body) == "function" then
-		return self.body()
+		return vim.trim(self.body())
 	end
 
 	return ""
